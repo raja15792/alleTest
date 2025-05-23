25bebb0dfb9ebea1277d8f166bbcabebab8d3618
Prakash Kushwaha <prakashveer15792@gmail.com>
2025-05-23 15:58:03 +0530

    Added echo framework and healthcheck route

--- src/main.go
+++ src/main.go
@@ -1,2 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance -> Using echo as a http routing 
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 2,
+	}))
+
+	hc := e.Group("/healthcheck")
+	hc.GET("/alive", ok)
+
+	log.Println("starting server...")
+
+	e.Logger.Panic(e.Start(fmt.Sprintf(":%s", "8080")))
+}
+
+func ok(c echo.Context) error {
+	return c.String(http.StatusOK, "ok")
+}
